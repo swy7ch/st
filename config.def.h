d82d53fcfbfe107550914fb226497f61147150b4
David JULIEN <swy7ch@protonmail.com>
2021-03-15 23:20:27 +0100

    feat: font2 patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,13 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"Symbols Nerd Font:size=11",
+};
+
 static int borderpx = 2;
 
 /*
